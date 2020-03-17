92dffa81bba666f221b0e1702bfb5e718b8e19e1
vBryann <vitorbryan_melo@hotmail.com>
2020-03-17 11:12:19 -0300

    Update na função criar

--- DailyTasks/DailyTasks/main.swift
+++ DailyTasks/DailyTasks/main.swift
@@ -15,7 +15,7 @@ public func menu (){
     Clique em alguma tecla para continuar.
 
 """)
-    if let something = readLine(){
+    if readLine() != nil  {
         print("""
 
     ---------------------Menu---------------------
@@ -48,42 +48,53 @@ func criarTask (){
     let formater = DateFormatter()
     formater.dateFormat = "HH:mm"
     var task: [(descricao: String, hora: String, tipo: String)] = []
-    print("Informe a descrição da task.")
+    
+    print("\nInforme a descrição da task.")
     if let descricaoInput = readLine(){
-    }
-    print("Informe o tipo da task. Ex: Estudos, Exercícios, Lazer etc.")
-    if let tipoInput = readLine(){
-    }
-    var dateAsString = "0:00"
-    print("Informe a hora da task. Ex: 12:00")
-    if let horaInput = readLine(){
-        var number: [Character] = []
-        for aux in horaInput {
-            number.append(aux)
-        }
-        let hour = String(number[0]) + String(number[1])
-        let minute = String(number[3]) + String(number[4])
         
-        let hourInt = Int(hour)
-        let minuteInt = Int(minute)
+        print("\nInforme o tipo da task. Ex: Estudos, Exercícios, Lazer etc.")
+        if let tipoInput = readLine(){
         
-        if (hourInt > 23)|| ( minuteInt > 59){
-            
+            var dateAsString = "0:00"
+            print("\nInforme a hora da task. Ex: 12:00")
+            while let horaInput = readLine(){
+                var number: [Character] = []
+                //separando os caracteres da hora para fazer o tratamento
+                for aux in horaInput {
+                    number.append(aux)
+                }
+                let hour = String(number[0]) + String(number[1])
+                let minute = String(number[3]) + String(number[4])
+               //transformando os chars separados em int
+                let hourInt = Int(hour)
+                let minuteInt = Int(minute)
+                
+                //comparação de hora invalida
+                if (hourInt! > 23) || ( minuteInt! > 59){
+                    print("\nHorário inválido, tente novamente.")
+                    continue
+                }else{
+                    dateAsString = horaInput
+                    guard let date = formater.date(from: dateAsString) else { return }
+                    //é um option, então é preciso tratar
+                    let hourFuture = Calendar.current.component(.hour, from: date)
+                    let minuteFuture = Calendar.current.component(.minute, from: date)
+                    // é possivel pegar a hora setada na variável date
+                    task.append((descricaoInput,horaInput,tipoInput))
+                    break
+                
+                }
+        
+            }
         }
-        //comparação de hora invalida
-        dateAsString = horaInput
     }
-    guard let date = formater.date(from: dateAsString) else { return }
-    //é um option, então é preciso tratar
-    let hourFuture = Calendar.current.component(.hour, from: date)
-    let minuteFuture = Calendar.current.component(.minute, from: date)
-    // é possivel pegar a hora setada na variável date
+   
 
     //print(date.description(with: Locale(identifier: "pt-br")))
     // converte  a hora no padrão pt-br
-    let datestr = formater.string(from: date)
-    print("hora em string: ", datestr)
-    print()
+    //let datestr = formater.string(from: date)
+    //print("hora em string: ", datestr)
+    //print()
     
     //let hour = Calendar.current.component(.hour, from: Date())
     //let minutes = Calendar.current.component(.minute, from: Date())
@@ -92,14 +103,14 @@ func criarTask (){
     //    let hour = components.hour ?? 0
     //    let minute = components.minute ?? 0
     
-    print()
+    //print()
     // cria um array de tasks organizadas em tuplas
-    task.append(("Ir pro academy", "7:00", "Faculdade"))
-    task.append(("Regar plantinhas", "15:00", "Casa"))
+    //task.append(("Ir pro academy", "7:00", "Faculdade"))
+    //task.append(("Regar plantinhas", "15:00", "Casa"))
     // teste pra adicionar algumas tasks
-    print(task[0].descricao)
+    //print(task[0].descricao)
     // imprime só a descriçao
-    //print(task) -- imprime tudo ex:(descricao: "Ir pro academy", hora: "7:00", tipo: "Faculdade")
+    print(task) //-- imprime tudo ex:(descricao: "Ir pro academy", hora: "7:00", tipo: "Faculdade")
 
 }
 
