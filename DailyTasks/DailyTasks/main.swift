205bba7b79ab72fe057df95002184c7065f1b191
vBryann <vitorbryan_melo@hotmail.com>
2020-03-30 19:44:20 -0300

    Update na função CriarTask utilizando FileManager

--- DailyTasks/DailyTasks/main.swift
+++ DailyTasks/DailyTasks/main.swift
@@ -2,52 +2,115 @@
 //  main.swift
 //  DailyTasks
 //
-//  Created by Brena Amorim on 10/03/20.
+//  Created by Brena Amorim and Vitor Bryan on 10/03/20.
 //  Copyright © 2020 Brena Amorim. All rights reserved.
 //
 
 import Foundation
 
 public func menu (){
+    var condition = true
     print("""
 
-    Bem vindo ao DailyTask, onde você pode organizar suas atividades diárias!
-    Clique em alguma tecla para continuar.
+        Bem vindo ao DailyTask, onde você pode organizar suas atividades diárias!
 
-""")
-    if readLine() != nil  {
-        print("""
+    """)
+    while condition{
+        print("\nClique em alguma tecla para continuar.")
+        if readLine() != nil  {
+            print("""
 
-    ---------------------Menu---------------------
+        ---------------------Menu---------------------
 
 
-    1. Criar Task
-    2. Editar Task
-    3. Completar Task
-    4. Listar Task
+        1. Criar Task
+        2. Editar Task
+        3. Completar Task
+        4. Listar Task
 
-    5. Sair
+        5. Sair
 
-    ----------------------------------------------
+        ----------------------------------------------
 
-""")
-        if let command = readLine(){
-            switch command{
-                case "1": criarTask()
-                case "5": break
-                default : print("N deu irmao, tente dnv")
+    """)
+            if let command = readLine(){
+                switch command{
+                    case "1": try? criarTask()
+                        var flag = true
+                        while flag {
+                            print("\nVocê quer continuar no programa? s/n")
+                            if let command2 = readLine(){
+                                switch command2{
+                                case "s","S":
+                                    condition = true
+                                    flag = false
+                                case "n","N":
+                                    condition = false
+                                    flag = false
+                                default:
+                                    print("Comando inválido")
+                                }
+                            }
+                        }
+                    //case "2":
+                    //editarTask()
+                    //condition = false
+                    case "5":
+                    condition = false
+                    break
+                    default : print("N deu irmao, tente dnv. Pressione alguma tecla pra continuar")
+                }
             }
+        
         }
-    
     }
 }
 
-func criarTask (){
+func criarTask () throws{
     
-    //print(Date().description(with: .current))
     let formater = DateFormatter()
     formater.dateFormat = "HH:mm"
-    var task: [(descricao: String, hora: String, tipo: String)] = []
+    enum PersistenceError: Error {
+       case deuRuimNaLeitura
+       case deuRuimNaEscrita
+    }
+   
+    struct PersistenceManager {
+       
+       //Funcao que acessa o diretório Documents no Mac
+       func getDocumentsDirectory() -> URL {
+           let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+           return paths[0]
+       }
+       //Funcao que persiste dados em um arquivo
+       func write(data: Data, toFile path: String) -> Bool {
+           let fm = FileManager.default
+           let filePath = getDocumentsDirectory().appendingPathComponent(path).relativePath
+           
+           //Cria um arquivo no diretório especificado, e tenta escrever o conteudo em formato Data nesse arquivo.
+           let sucess = fm.createFile(atPath: filePath, contents: data, attributes: nil)
+           return sucess // retorna se a escrita foi bem sucedida
+       }
+       
+       //Funcao que ler dados de um arquivo
+       func read(fromFile filename: String) -> Data? {
+           let fm = FileManager.default
+           let path = getDocumentsDirectory().appendingPathComponent(filename).relativePath
+           
+           //leitura do conteúdo de um diretório
+           let data = fm.contents(atPath: path)
+           return data //retorna o conteúdo lido
+       }
+       
+    }
+    
+    struct Task: Codable{
+        var descricao: String
+        var tipo: String
+        var hora: String
+        
+    }
+    
     
     print("\nInforme a descrição da task.")
     if let descricaoInput = readLine(){
@@ -55,7 +118,7 @@ func criarTask (){
         print("\nInforme o tipo da task. Ex: Estudos, Exercícios, Lazer etc.")
         if let tipoInput = readLine(){
         
-            var dateAsString = "0:00"
+            //var dateAsString = "0:00"
             print("\nInforme a hora da task. Ex: 12:00")
             while let horaInput = readLine(){
                 var number: [Character] = []
@@ -72,15 +135,31 @@ func criarTask (){
                 //comparação de hora invalida
                 if (hourInt! > 23) || ( minuteInt! > 59){
                     print("\nHorário inválido, tente novamente.")
+                    //Pode ser mudado pra tratamento de errors
                     continue
                 }else{
-                    dateAsString = horaInput
-                    guard let date = formater.date(from: dateAsString) else { return }
+                    //dateAsString = horaInput
+                    //guard let date = formater.date(from: dateAsString) else { return }
                     //é um option, então é preciso tratar
-                    let hourFuture = Calendar.current.component(.hour, from: date)
-                    let minuteFuture = Calendar.current.component(.minute, from: date)
+                    //let hourFuture = Calendar.current.component(.hour, from: date)
+                    //let minuteFuture = Calendar.current.component(.minute, from: date)
                     // é possivel pegar a hora setada na variável date
-                    task.append((descricaoInput,horaInput,tipoInput))
+                    //task.append((descricaoInput,tipoInput,horaInput))
+                    let manager = PersistenceManager()
+                    
+                    let task = Task(descricao:descricaoInput,tipo:tipoInput,hora:horaInput)
+                    
+                    let encoder = JSONEncoder()
+                    if let encoded = try? encoder.encode(task){
+                        //Aqui faz o encoding da Struct
+                        if let json = String(data: encoded, encoding: .utf8) {
+                            guard let data = json.data(using: .utf8) else{
+                                print("Converter content para Data retornou nulo!")
+                                throw PersistenceError.deuRuimNaLeitura
+                            }
+                            let success = manager.write(data: data, toFile: "arquivo.json")
+                            print("Bem sucedido:",success)
+                        }
                     break
                 
                 }
@@ -88,8 +167,6 @@ func criarTask (){
             }
         }
     }
-   
-
     //print(date.description(with: Locale(identifier: "pt-br")))
     // converte  a hora no padrão pt-br
     //let datestr = formater.string(from: date)
@@ -110,9 +187,44 @@ func criarTask (){
     // teste pra adicionar algumas tasks
     //print(task[0].descricao)
     // imprime só a descriçao
-    print(task) //-- imprime tudo ex:(descricao: "Ir pro academy", hora: "7:00", tipo: "Faculdade")
-
+    //print(task) //-- imprime tudo ex:(descricao: "Ir pro academy", hora: "7:00", tipo: "Faculdade")
+   
+    
 }
+/*func editarTask () throws{
+           let decoder = JSONDecoder()
+        //Aqui faz o decoding da Struct
+           if let decoded = try? decoder.decode(Task.self, from: encoded) {
+               print(decoded)
+           }
+    }*/
+    // Aqui, no caso de JSON, você usa o JSONEncoder() pra converter uma Struct que está em conformidade com o protocolo Encodable para o tipo Data?. O guard let darante que essa conversão deu certo e Data não é nil. Lembrando que Codable é a junção de ambos protocolos, Encodable e Decodable.
+
+    //Conversão da String content para o tipo Data
+   
+
+    // chamando a função write para criar um arquivo
+
+    // chamando a função read para ler o conteúdo do arquivo e retorná-lo em formato Data?
+    //let readData: Data? = manager.read(fromFile: "arquivo.txt")
+
+    //Aqui você pode usar o data pra construir um struct com JSONDecoder() e retorná-lo na função.
+    //Decode Data? -> AlgumaStruct, essa struct sendo Decodable. Lembrando que Codable é Encodable e Decodable ao mesmo tempo.
+
+    //Nesse exemplo, estamos lendo e retornando dados do tipo String
+    //Se data nao for nula e for convertível para String, converte pra string e printa
+    //if let readData = readData, let stringFromData = String(data: readData, encoding: .utf8) {
+      //  print(stringFromData)
+    //} else {
+        //Se nao, printa que houve algum problema
+    //    print("Nao foi possivel converter data para o tipo String!")
+    //}
+    
+}
+
+
+
+
 
 menu()
 
